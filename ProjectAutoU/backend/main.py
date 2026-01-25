d3476ac16571065aa1371ca90ac86b673697fe52
Diego Carvalho <diegoricardo2527@gmail.com>
2026-01-25 17:19:48 -0300

    feat: validar integração do back-end com front-end e testes das rotas

--- ProjectAutoU/backend/main.py
+++ ProjectAutoU/backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.api.rotas_email import router as rotas_email
 
 app = FastAPI(
     title="API de Análise de Emails",
     description="API para classificar emails e sugerir respostas",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(rotas_email)
